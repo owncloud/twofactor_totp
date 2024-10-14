ce90be0fbc3eac534a4c013da249dcb701f57bac
ownClouders <devops@owncloud.com>
2024-10-14 01:39:00 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "twofactor_totp",
+    {
+    "Activate TOTP" : "TOTP نى قوزغىتىڭ",
+    "This is your new TOTP secret:" : "بۇ سىزنىڭ يېڭى TOTP مەخپىيىتىڭىز:",
+    "Scan this QR code with your TOTP app" : "بۇ QR كودىنى TOTP ئەپىڭىز بىلەن سايىلەڭ",
+    "To enable second-factor verify authentication code below." : "تۆۋەندىكى ئىككىنچى ئامىلنى دەلىللەش كودىنى قوزغىتىش.",
+    "Authentication code" : "دەلىللەش كودى",
+    "Verify" : "دەلىللەڭ",
+    "Verified" : "دەلىللەندى",
+    "Not verified" : "دەلىللەنمىدى",
+    "Time-based One-time Password" : "ۋاقىتنى ئاساس قىلغان بىر قېتىم پارول",
+    "Authenticate with a TOTP app" : "TOTP دېتالى ئارقىلىق دەلىللەڭ",
+    "Scan the QR code below with your TOTP app and enter the code" : "تۆۋەندىكى QR كودىنى TOTP ئەپىڭىز بىلەن سايىلەپ كودنى كىرگۈزۈڭ",
+    "TOTP Second-factor Auth" : "TOTP ئىككىنچى ئامىل Auth"
+},
+"nplurals=2; plural=(n != 1);");
